39fd28d016ead991f281a1b5ae3d6d5cb4fc3c22
Mike <mlaitw@gmail.com>
2023-12-09 18:03:58 +0800

    read file into a string

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # txt2csv
 
-transform a txt file which contents in fixed format into a csv file
+Transform a txt file which contents in fixed format into a csv file.
+
+## about data.txt
+
+Data.txt added to .gitignore due to privacy.
+the content look like below:
+
+Google mail
+  accout
+  password
+
+Riot game
+  related to google account
--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-const fs = require('node:fs/promise')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+
+const chardet = require('chardet')
+const { Buffer } = require('node:buffer')
+const iconv = require('iconv-lite') 
+
 const path = 'data.txt'
+console.log(`encoding of ${path}: ${chardet.detectFileSync(path)}\n`)
+
+//promise API
+let fileInString = ''
+readFilePromise(path).then(() => console.log(fileInString))
+
+async function readFilePromise(path) {
+  let data = await fsPromises.readFile(path)
+  fileInString = iconv.decode(Buffer.from(data), 'big5')
+}
+
+
+//synchronous API
+
+// let data = fs.readFileSync(path)
+// data = iconv.decode(Buffer.from(data), 'big5') 
+// console.log(data)
+
+// function readFile(path) {
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       console.err(err)
+//       return
+//     }
+//     let output = iconv.decode(Buffer.from(data), 'big5')
+//     console.log(output)
+//     return output
+//   })
+// }
